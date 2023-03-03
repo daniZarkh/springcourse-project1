840f51101adb9ee34eb30034998d45bfb6d703a0
noslav7 <noslav7@gmail.com>
2023-03-03 16:41:05 +0500

    small fix

--- src/main/java/ru/alishev/springcourse/controllers/BooksController.java
+++ src/main/java/ru/alishev/springcourse/controllers/BooksController.java
@@ -1,5 +1,6 @@
 package ru.alishev.springcourse.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -9,15 +10,18 @@ import ru.alishev.springcourse.dao.PersonDAO;
 import ru.alishev.springcourse.models.Book;
 import ru.alishev.springcourse.models.Person;
 
+import javax.validation.Valid;
 import java.util.Optional;
 
 @Controller
 @RequestMapping("/books")
-public class BookController {
+public class BooksController {
+
     private final BookDAO bookDAO;
     private final PersonDAO personDAO;
 
-    public BookController(BookDAO bookDAO, PersonDAO personDAO) {
+    @Autowired
+    public BooksController(BookDAO bookDAO, PersonDAO personDAO) {
         this.bookDAO = bookDAO;
         this.personDAO = personDAO;
     }
@@ -31,12 +35,14 @@ public class BookController {
     @GetMapping("/{id}")
     public String show(@PathVariable("id") int id, Model model, @ModelAttribute("person") Person person) {
         model.addAttribute("book", bookDAO.show(id));
+
         Optional<Person> bookOwner = bookDAO.getBookOwner(id);
-        if (bookOwner.isPresent()) {
+
+        if (bookOwner.isPresent())
             model.addAttribute("owner", bookOwner.get());
-        } else {
+        else
             model.addAttribute("people", personDAO.index());
-        }
+
         return "books/show";
     }
 
@@ -46,12 +52,28 @@ public class BookController {
     }
 
     @PostMapping()
-    public String create(@ModelAttribute("book") Book book,
+    public String create(@ModelAttribute("book") @Valid Book book,
                          BindingResult bindingResult) {
         if (bindingResult.hasErrors()) {
             return "books/edit";
         }
         bookDAO.save(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{id}/edit")
+    public String edit(Model model, @PathVariable("id") int id) {
+        model.addAttribute("book", bookDAO.show(id));
+        return "books/edit";
+    }
+
+    @PatchMapping("/{id}")
+    public String update(@ModelAttribute("book") @Valid Book book, BindingResult bindingResult,
+                         @PathVariable("id") int id) {
+        if (bindingResult.hasErrors())
+            return "books/edit";
+
+        bookDAO.update(id, book);
         return "redirect:/books";
     }
 
@@ -68,8 +90,8 @@ public class BookController {
     }
 
     @PatchMapping("/{id}/assign")
-    public String assign(@PathVariable("id") int id, @ModelAttribute("Person") Person selectedPerson) {
+    public String assign(@PathVariable("id") int id, @ModelAttribute("person") Person selectedPerson) {
         bookDAO.assign(id, selectedPerson);
-        return "redirect:/books" + id;
+        return "redirect:/books/" + id;
     }
 }
--- src/main/java/ru/alishev/springcourse/dao/BookDAO.java
+++ src/main/java/ru/alishev/springcourse/dao/BookDAO.java
@@ -1,18 +1,20 @@
 package ru.alishev.springcourse.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.alishev.springcourse.models.Book;
 import ru.alishev.springcourse.models.Person;
-
 import java.util.List;
 import java.util.Optional;
 
 @Component
 public class BookDAO {
+
     private final JdbcTemplate jdbcTemplate;
 
+    @Autowired
     public BookDAO(JdbcTemplate jdbcTemplate) {
         this.jdbcTemplate = jdbcTemplate;
     }
@@ -22,7 +24,7 @@ public class BookDAO {
     }
 
     public Book show(int id) {
-        return jdbcTemplate.query("SELECT FROM book WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
+        return jdbcTemplate.query("SELECT * FROM book WHERE id=?", new Object[]{id}, new BeanPropertyRowMapper<>(Book.class))
                 .stream().findAny().orElse(null);
     }
 
